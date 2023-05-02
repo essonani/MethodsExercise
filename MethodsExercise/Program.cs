7ef94fa7d6c116ff73116d5b55dd86d314002a5e
essonani tchakpalla <essonani@hotmail.com>
2023-05-01 19:17:48 -0500

    sgsh

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("what is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal");
+            string animal = Console.ReadLine();
+            Console.WriteLine("what is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine("your name is " + name + " and your favorite color is " + color + " you like " + animal + band + " is your favorite band ");
         }
     }
 }
